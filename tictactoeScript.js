1fe6670f52fbf4c48727b50189d52ed9737b4e30
DutchNukeTD <golan93@hotmail.com>
2024-04-28 17:39:02 +1000

    First version is finised. With a working html and console.log. Not sure if it's perfectly done as the exercise. Maybe for futre update: play against the computer and cleanup code. git push origin main git status :

--- tictactoeScript.js
+++ tictactoeScript.js
@@ -1,18 +1,26 @@
 let gameboard = function(){
     let board = [[""],[""],[""],
                 [""],[""],[""],
-                [""],[""],[""]]    
+                [""],[""],[""]]
 
     return function(){
         return board;
     };
 }();
 
+// Exicuted when game.end(); 
+function resetGB(gameboard){
+    for (let i = 0; i < gameboard().length; i++) {
+        gameboard()[i] = '';
+    }
+    return gameboard;
+};
+
 function returnGB(gameboard) {
     console.log('[' + gameboard()[0] + ']' + '[' + gameboard()[1] + ']' + '[' + gameboard()[2] + ']' );
     console.log('[' + gameboard()[3] + ']' + '[' + gameboard()[4] + ']' + '[' + gameboard()[5] + ']' );
     console.log('[' + gameboard()[6] + ']' + '[' + gameboard()[7] + ']' + '[' + gameboard()[8] + ']' );
-    // return gameboard;
+    return gameboard;
 };
 
 // Create player object
@@ -58,6 +66,11 @@ let player = {
         return this.value;
     }, 
 
+    // resetValue = game always starts with player 'X' first. 
+    resetValue: function(){
+        return this.value = 'O';
+    },
+
     getMove: function(){
         let move = prompt(this.value + 'Where do you place your mark?'); 
         if (move === null){
@@ -69,11 +82,17 @@ let player = {
 };
 
 function addMove( playerValue, playerMove ) {
-    gameboard()[playerMove] = playerValue;
-    return gameboard;
-}
-
-// should be --> addMove( player.getValue(), player.getMove() );
+    // Check if gameboard spot is empty
+    if (gameboard()[playerMove] == ''){
+        gameboard()[playerMove] = playerValue;
+        return gameboard;
+    } else if (playerMove == null) {
+        game.end();
+    } else { // gameboard spot is already taken, ask again. 
+        console.log('This spot is already taken, choose a different one!');
+        addMove( playerValue, player.getMove() );
+    }
+};
 
 // Check for a draw
 function checkForDraw(gameboard){
@@ -89,48 +108,74 @@ function checkForDraw(gameboard){
     if (gameOptions == gameboard().length){
         returnGB(gameboard);
         console.log("it's a draw, game over!");
+        dialogText.innerHTML = "It's a draw, game over!";
+        dialog.showModal();
         game.end();
     }
 };
 
-// All game solutions
-verticalOne = [gameboard[0], gameboard[1], gameboard[2]];
-verticalTwo = [gameboard[3], gameboard[4], gameboard[5]];
-verticalThree = [gameboard[6], gameboard[7], gameboard[8]];
+function getGB() {
+    // All game solutions
+    verticalOne = [gameboard()[0], gameboard()[1], gameboard()[2]];
+    verticalTwo = [gameboard()[3], gameboard()[4], gameboard()[5]];
+    verticalThree = [gameboard()[6], gameboard()[7], gameboard()[8]];
 
-horizontallOne = [gameboard[0], gameboard[3], gameboard[6]];
-horizontalTwo = [gameboard[1], gameboard[4], gameboard[7]];
-horizontalThree = [gameboard[2], gameboard[5], gameboard[8]];
+    horizontallOne = [gameboard()[0], gameboard()[3], gameboard()[6]];
+    horizontalTwo = [gameboard()[1], gameboard()[4], gameboard()[7]];
+    horizontalThree = [gameboard()[2], gameboard()[5], gameboard()[8]];
 
-diagonalOne = [gameboard[0], gameboard[4], gameboard[8]];
-diagonalTwo = [gameboard[2], gameboard[4], gameboard[6]];
+    diagonalOne = [gameboard()[0], gameboard()[4], gameboard()[8]];
+    diagonalTwo = [gameboard()[2], gameboard()[4], gameboard()[6]];
 
-// Array with each solution. 
-solutions = [ verticalOne, verticalTwo, verticalThree, horizontallOne, horizontalTwo, horizontalThree, diagonalOne, diagonalTwo ];
+    // Array with each solution. 
+    solutions = [ verticalOne, verticalTwo, verticalThree, horizontallOne, horizontalTwo, horizontalThree, diagonalOne, diagonalTwo ];
+    return solutions;
+};
 
 
 // Loop thru solutions to check if the game has been won. 
 function checkSolutions(){
+    getGB();
     for (let option = 0; option < solutions.length; option++){
         countX = 0;
         countO = 0;
         for (let i = 0; i < solutions[option].length; i++){
-            if (solutions[option][i] == 'x') {
+            if (solutions[option][i] == 'X') {
                 countX = countX +1; 
-            } else if (solutions[option][i] == 'o') {
+            } else if (solutions[option][i] == 'O') {
                 countO = countO +1; 
             }
         }
         if (countX == 3){
-            console.log('X has won!');
+            returnGB(gameboard);
+            let nameX = document.querySelector('#PlayerOne');
+            if (nameX.value == '') {
+                console.log('X has won!');
+                dialogText.innerHTML = "X has won!";
+                dialog.showModal();
+            } else {
+                console.log(nameX.value + ' has won!');
+                dialogText.innerHTML = nameX.value+ " has won!";
+                dialog.showModal();
+            }
             game.end();
         } else if (countO == 3) {
-            console.log('O has won!');
+            returnGB(gameboard);
+            let nameO = document.querySelector('#PlayerTwo');
+            if (nameO.value == '') {
+                console.log('O has won!');
+                dialogText.innerHTML = "O has won!";
+                dialog.showModal();                
+            } else {
+                console.log(nameO.value + ' has won!');
+                dialogText.innerHTML = nameO.value+ " has won!";
+                dialog.showModal();
+            }
             game.end();
         }
     }
+    // console.log('Check done!'); conform that check is done. 
 };
-// checkSolutions();
 
 // let startGame = true;  --> niet nodig?
 const game = {
@@ -138,28 +183,58 @@ const game = {
         return startGame = true;
     }, 
     end: function(){
+        resetGB(gameboard); // reset gameboard for new game. 
+        player.resetValue(); // Set first player back to 'X'
         return startGame = false;
     }
 }
 
 
 function GameControl() {
-    // start Game
-    // while true game keeps running
-    // let startGame = function(){
-    //     return true;
-    // }
-
     game.start();
 
     while(startGame == true){
         // create gameboard
         returnGB(gameboard);
-        addMove( player.getValue(), player.getMove() );
+        // addMove( player.getValue(), player.getMove() );
         checkSolutions(gameboard);
         checkForDraw(gameboard);
-
     }
 };
 
-GameControl();
+// GameControl(); // Aan vanaf het begin?
+
+
+/////////////////////////////////////////////////////////
+// HTML INTERACTION //
+/////////////////////////////////////////////////////////
+
+const buttons = document.querySelectorAll('.Button');
+buttons.forEach((button) => {
+    button.addEventListener("click", (event)=>{
+        if (event.target.classList.contains('Button')) {
+            playerValue = player.getValue();
+            buttonNumber = button.className.split('_')[1];
+            if (gameboard()[buttonNumber] == ''){
+                gameboard()[buttonNumber] = playerValue;
+                button.innerHTML = playerValue;
+                checkSolutions(gameboard);
+                checkForDraw(gameboard);
+            } else { // gameboard spot is already taken, ask again. 
+                player.getValue(); // keeping the same player value!
+            }
+        }
+    })
+});
+
+const dialog = document.querySelector('.dialog');
+let dialogText = document.querySelector('.dialogText');
+
+const dialogButton = document.querySelector('.dialogButton');
+dialog.addEventListener("click", (event) =>{
+    resetGB(gameboard);
+    buttons.forEach((button) => {
+        button.innerHTML = "";
+    })
+    dialog.close();
+});
